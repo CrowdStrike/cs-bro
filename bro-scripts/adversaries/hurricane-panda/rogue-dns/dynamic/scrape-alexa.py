0fbf94bda0b602743931713648a04b28dba61517
jshlbrd <liburdi.joshua@gmail.com>
2015-01-20 15:13:17 -0800

    Create scrape-alexa.py

---
+++ bro-scripts/adversaries/hurricane-panda/rogue-dns/dynamic/scrape-alexa.py
@@ -0,0 +1,23 @@
+# Rudimentary script to collect domains in the Alexa top 500
+# This script can be run as often as needed to refresh the list of domains
+# CrowdStrike 2014
+# josh.liburdi@crowdstrike.com
+
+import requests
+import bs4
+
+# File containing Alexa top 500 domains
+# This file name and path is referenced in the Bro script and can be modified
+f = open('alexa_domains.txt','w')
+f.write('#fields\talexa\n')
+
+# Alexa's top 500 domains are spread across 20 pages
+# To change the number of domains collected (top 50, top 250), modify the range
+for num in range(0,20):
+  site = "http://www.alexa.com/topsites/global;" + str(num)
+  page = requests.get(site)
+  soup = bs4.BeautifulSoup(page.text)
+
+  for link in soup.find_all('a'):
+    if 'siteinfo' in str(link):
+      f.write((link.get('href')).split("/")[2] + "\n" )
